26d755b6f2b2076ced3a09ebc9895ba92a8214f7
Yousra <110408013+yousrachakib@users.noreply.github.com>
2023-02-17 19:54:29 +0100

    test

--- sort100numbers.c
+++ sort100numbers.c
@@ -6,7 +6,7 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 17:40:37 by yochakib          #+#    #+#             */
-/*   Updated: 2023/02/13 21:26:28 by yochakib         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:27:58 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 void	sort_100numbers(t_list	**stack_a, t_list	**stack_b)
 {
-	int min;
-	int pos;
-	
-	while(*stack_a)
-	{
-		min = return_minvalue(*stack_a);
-		pos = get_position(*stack_a, min);
-		while((*stack_a)->data != min)
-		{
-			if (pos > (list_size(*stack_a) / 2))
-				rrotate_stack(stack_a, 1);
-			else
-				rotate_stack(stack_a, 1);
-		}
-		push(stack_a, stack_b, 1);
-	}
-	while (*stack_b != NULL)
-		push(stack_a, stack_b, 0);
-}
+	int chunk;
+	int nextchunk;
+	int i;
 
+	i = 0;
+	chunk = list_size(*stack_a);
+	nextchunk = chunk;
+
+	while (*stack_a)
+	{
+		while ((*stack_a) != NULL && (*stack_a)->index <= chunk)
+		{
+			push(stack_a, stack_b, 1);
+			i++;
+			if (i > chunk)
+				chunk += nextchunk;
+		}
+		rotate_stack(stack_a, 1);
+	}
+}
