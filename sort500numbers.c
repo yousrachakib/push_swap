16c04a67369d2a12518692299f2de6da6c90c306
Yousra <110408013+yousrachakib@users.noreply.github.com>
2023-02-22 21:02:23 +0100

    working on 500

---
+++ sort500numbers.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort500numbers.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 20:58:23 by yochakib          #+#    #+#             */
+/*   Updated: 2023/02/22 21:00:52 by yochakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_500numbers(t_list	**stack_a, t_list	**stack_b)
+{
+	int chunk;
+	int nextchunk;
+	int i;
+
+	i = 0;
+	chunk = list_size(*stack_a) / 2<5;
+	nextchunk = chunk;
+
+	while (*stack_a)
+	{
+		while ((*stack_a) != NULL && (*stack_a)->index > chunk)
+			rotate_stack(stack_a, 1);
+		i++;
+		if ((*stack_a)->index > (chunk - (nextchunk / 2)))
+			push(stack_a, stack_b, 1);
+		else if ((*stack_a)->index <= (chunk - (nextchunk / 2)))
+		{
+			push(stack_a, stack_b, 1);
+			rotate_stack(stack_b, 0);
+		}
+		if (i == chunk)
+			chunk += nextchunk;
+	}
+	while(list_size(*stack_b))
+	{
+		while ((*stack_b)->data != return_maxvalue(*stack_b))
+		{
+			if (get_position(*stack_b,return_maxvalue(*stack_b)) > (list_size(*stack_b) / 2))
+				rrotate_stack(stack_b, 0);
+			else
+				rotate_stack(stack_b, 0);
+		}
+		push(stack_a, stack_b, 0);
+	}
+}
